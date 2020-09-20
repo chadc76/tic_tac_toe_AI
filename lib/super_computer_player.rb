211d623e0522943694291a787877858849caa4ab
Chad Cunningham <chadcunningham7676@gmail.com>
2020-09-20 12:28:46 -0400

    move method and completed Phase II: SuperComputerPlayer

--- lib/super_computer_player.rb
+++ lib/super_computer_player.rb
@@ -2,6 +2,15 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    possible_moves = node.children.shuffle
+    possible_moves.each do |child|
+      return child.prev_move_pos if child.winning_node?(mark)
+    end
+    possible_moves.each do |child|
+      return child.prev_move_pos if !child.losing_node?(mark)
+    end
+    Raise "There are no non-losing nodes!"
   end
 end
 
