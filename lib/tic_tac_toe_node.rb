4da147aff5405a2cf275692628e64cb1e0776ce5
Chad Cunningham <chadcunningham7676@gmail.com>
2020-09-20 11:32:37 -0400

    Added losing_node? method

--- lib/tic_tac_toe_node.rb
+++ lib/tic_tac_toe_node.rb
@@ -9,6 +9,12 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
+    return (board.won? && board.winner != evaluator) if board.over?
+    if self.next_mover_mark == evaluator
+      self.children.all?{ |node| node.losing_node?(evaluator) }
+    else
+      self.children.any?{ |node| node.losing_node?(evaluator) }
+    end
   end
 
   def winning_node?(evaluator)
